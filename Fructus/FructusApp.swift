3d95e4c57ce892b1ac6de1bce0f7ec4c37dba821
Rakibul Huda <rakib10rr3@gmail.com>
2021-01-06 21:09:17 +0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rakibul Huda on 6/1/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
